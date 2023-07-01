2d4dd61c945d0c4c59f7501020ea3fb66f40d541
Nakupenda <116508057+Nakupenda25@users.noreply.github.com>
2023-07-01 12:28:23 +0200

    Add Javadoc comments for Liquid class

--- src/main/java/at/fhj/msd/Liquid.java
+++ src/main/java/at/fhj/msd/Liquid.java
@@ -1,42 +1,89 @@
 package at.fhj.msd;
-// put into drinkss
+
+/**
+ * Class represents a liquid which can be used in drinks
+ */
 public class Liquid {
 
+    /**
+     * name of liquid
+     */
     private String name;
-    // volume in l
-     
+
+    /**
+     * volume of liquid in litre
+     */
     private double volume;
 
-    // alcohohl percent (eg 42 ) 
 
+    /**
+     * alcohol volume percent of liquid
+     */
     private double alcoholPercent;
 
+    /**
+     * Creates a Liquid object with given name, volume and alcoholPercent
+     *
+     * @param name name of liquid
+     * @param volume volume of liquid in litre
+     * @param alcoholPercent alcohol volume percent of liquid
+     */
     public Liquid(String name, double volume, double alcoholPercent) {
         this.name = name;
         this.volume = volume;
         this.alcoholPercent = alcoholPercent;
     }
 
+    /**
+     * Getter for name
+     *
+     * @return name of liquid
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for name
+     *
+     * @param name name of liquid
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for volume
+     *
+     * @return volume of liquid in litre
+     */
     public double getVolume() {
         return volume;
     }
 
+    /**
+     * Setter for volume
+     *
+     * @param volume volume of liquid in litre
+     */
     public void setVolume(double volume) {
         this.volume = volume;
     }
 
+    /**
+     * Getter for alcoholPercent
+     *
+     * @return alcohol volume percent of liquid
+     */
     public double getAlcoholPercent() {
         return alcoholPercent;
     }
- 
+
+    /**
+     * Setter for alcoholPercent
+     *
+     * @param alcoholPercent alcohol volume percent of liquid
+     */
     public void setAlcoholPercent(double alcoholPercent) {
         this.alcoholPercent = alcoholPercent;
     }
