8eb853ff9215235d9989b0ce53cd4caecc34a1ea
Daniel Zarnhofer <daniel.zarnhofer@edu.fh-joanneum.at>
2023-07-01 13:29:57 +0200

    create DrinkQueue

---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,20 @@
+package at.fhj.msd;
+
+public class DrinkQueue implements DrinkQueueInterface{
+
+
+    @Override
+    public void add(Drink drink) {
+
+    }
+
+    @Override
+    public Drink peek() {
+        return null;
+    }
+
+    @Override
+    public Drink remove() {
+        return null;
+    }
+}
