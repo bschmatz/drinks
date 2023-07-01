eea434adfae63509a598ede3eae034a5d9ba9d96
Daniel Zarnhofer <daniel.zarnhofer@edu.fh-joanneum.at>
2023-07-01 13:35:11 +0200

    Create DoubleQueue.java

---
+++ src/main/java/at/fhj/msd/DoubleQueue.java
@@ -0,0 +1,18 @@
+package at.fhj.msd;
+
+public class DoubleQueue implements DoubleQueueInterface{
+    @Override
+    public boolean add(Double obj) {
+        return false;
+    }
+
+    @Override
+    public Double peek() {
+        return null;
+    }
+
+    @Override
+    public Double remove() {
+        return null;
+    }
+}
